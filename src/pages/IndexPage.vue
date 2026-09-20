81af363abaabb4b7a6f64f83595ac4961433047d
Istiak95 <istiakabedin614@gmail.com>
2026-09-20 17:06:21 +0600

    Update creative portfolio page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,64 +1,47 @@
 <template>
-  <div class="q-pa-md flex flex-center full-height">
+  <div class="q-pa-lg bg-grey-3">
 
-    <div class="row full-width" style="max-width: 1400px;">
+    <div
+      class="row q-col-gutter-lg justify-center"
+      style="max-width: 1500px; margin: auto;"
+    >
 
-      <div class="col-8">
+      <!-- LEFT SIDE : COVER PAGE -->
+      <div class="col-12 col-md-5">
 
-        <q-card class="left-card">
+        <q-card class="cover-card q-pa-md">
 
-          <div class="row items-center q-pa-md">
+          <div class="cover-image-area">
 
-            <div class="logo q-mr-lg">
-              <div class="text-purple q-gutter-md" style="font-size: 2em">
-                <q-icon name="shopping_cart" color="red-5" size="40px" />
+            <q-img
+              :src="Image1"
+              class="cover-image"
+              fit="cover"
+            />
+
+            <!-- Black vertical title box -->
+            <div class="black-box">
+
+              <div class="portfolio-title">
+                CREATIVE PORTFOLIO
               </div>
-            </div>
-
-            <div class="row items-center q-gutter-md">
-
-              <q-btn flat label="SERVICES" no-caps />
-              <q-btn flat label="OFFICES" no-caps />
-              <q-btn flat label="TEAM" no-caps />
-              <q-btn flat label="PHILOSOPHY" no-caps />
-              <q-btn flat label="SPACES" no-caps />
 
             </div>
 
           </div>
 
+          <!-- Bottom Text -->
+          <div class="row justify-between q-mt-md q-px-sm">
 
-          <div class="image-area">
-
-            <div class="circle"></div>
-
-            <img
-             :src="Image1"
-             class="person-image"
-            alt="Person"
-          />
-
-          </div>
-
-
-          <div class="team">
-
-            <div>OUR TEAM</div>
-
-            <q-separator class="title-line" />
-
-            <div class="name">
-              Istiak Abedin
+            <div class="text-caption">
+              Design By:
+              <br>
+              <b>Istiak</b>
             </div>
 
-            <div class="job">
-              Machine Learning<br>
-              Researcher
+            <div class="text-caption">
+              2026
             </div>
-
-            <a href="#">
-              Explore
-            </a>
 
           </div>
 
@@ -67,45 +50,288 @@
       </div>
 
 
-      <div class="col-4">
+      <!-- RIGHT SIDE -->
+      <div class="col-12 col-md-7">
 
-        <q-card class="right-card">
+        <div class="row q-col-gutter-md">
 
-          <q-btn
-            outline
-            color="white"
-            label="LOGIN"
-            no-caps
-            class="login-btn"
-          />
+          <!-- PAGE 1 -->
+          <div class="col-12 col-sm-4">
 
-          <div class="content">
+            <q-card class="page-card q-pa-md">
 
-            <div class="title">
-              Your legal advisers
-              <br>
-              for the problems
-              <br>
-              of this century
-            </div>
+              <div class="text-h6 text-weight-bold">
+                INDEX
+              </div>
 
-            <div class="subtitle">
-              We offer solutions adapted to
-              <br>
-              today's needs
-            </div>
+              <q-separator class="q-my-sm" />
 
-            <q-btn
-              color="white"
-              text-color="primary"
-              label="VIEW MORE"
-              no-caps
-              class="view-btn"
-            />
+              <div class="text-caption q-mb-xs">
+                ABOUT THE DESIGN
+              </div>
+
+              <div class="text-caption q-mb-xs">
+                TEAM
+              </div>
+
+              <div class="text-caption q-mb-xs">
+                MODERN FASHION
+              </div>
+
+              <div class="text-caption q-mb-xs">
+                NEW COLLECTION
+              </div>
+
+              <div class="text-caption q-mb-xs">
+                INTERIORS
+              </div>
+
+              <q-img
+                :src="Image1"
+                class="small-image q-mt-md"
+                fit="cover"
+              />
+
+            </q-card>
 
           </div>
 
-        </q-card>
+
+          <!-- PAGE 2 -->
+          <div class="col-12 col-sm-4">
+
+            <q-card class="page-card q-pa-md">
+
+              <div class="text-subtitle1 text-weight-bold">
+                About The Studio
+              </div>
+
+              <div class="text-caption q-mt-sm">
+                Creative portfolio and visual design studio.
+                We create modern and minimal projects.
+              </div>
+
+              <q-img
+                :src="Image1"
+                class="medium-image q-mt-md"
+                fit="cover"
+              />
+
+            </q-card>
+
+          </div>
+
+
+          <!-- PAGE 3 -->
+          <div class="col-12 col-sm-4">
+
+            <q-card class="page-card q-pa-md">
+
+              <div class="text-subtitle1 text-weight-bold">
+                Team
+              </div>
+
+              <div class="text-caption q-mt-sm">
+                Creative Director
+              </div>
+
+              <q-img
+                :src="Image1"
+                class="team-image q-mt-md"
+                fit="cover"
+              />
+
+              <div class="text-caption q-mt-sm">
+                Photographer
+              </div>
+
+              <q-img
+                :src="Image1"
+                class="team-image q-mt-sm"
+                fit="cover"
+              />
+
+            </q-card>
+
+          </div>
+
+
+          <!-- PAGE 4 -->
+          <div class="col-12 col-sm-4">
+
+            <q-card class="page-card q-pa-md">
+
+              <div class="text-subtitle1 text-weight-bold">
+                Modern Fashion Show
+              </div>
+
+              <q-img
+                :src="Image1"
+                class="large-image q-mt-md"
+                fit="cover"
+              />
+
+              <div class="text-caption q-mt-md">
+                Modern fashion focuses on clean design,
+                simple colours and creative photography.
+              </div>
+
+            </q-card>
+
+          </div>
+
+
+          <!-- PAGE 5 -->
+          <div class="col-12 col-sm-4">
+
+            <q-card class="page-card q-pa-md">
+
+              <div class="text-subtitle1 text-weight-bold">
+                Modern Fashion
+              </div>
+
+              <div class="text-caption q-mt-sm">
+                Contemporary visual concepts and creative styling.
+              </div>
+
+              <div class="row q-col-gutter-sm q-mt-sm">
+
+                <div class="col-6">
+                  <q-img
+                    :src="Image1"
+                    class="grid-image"
+                    fit="cover"
+                  />
+                </div>
+
+                <div class="col-6">
+                  <q-img
+                    :src="Image1"
+                    class="grid-image"
+                    fit="cover"
+                  />
+                </div>
+
+              </div>
+
+            </q-card>
+
+          </div>
+
+
+          <!-- PAGE 6 -->
+          <div class="col-12 col-sm-4">
+
+            <q-card class="page-card q-pa-md">
+
+              <div class="text-subtitle1 text-weight-bold">
+                New Collection
+              </div>
+
+              <div class="row q-col-gutter-sm q-mt-md">
+
+                <div class="col-7">
+
+                  <q-img
+                    :src="Image1"
+                    class="collection-image"
+                    fit="cover"
+                  />
+
+                </div>
+
+                <div class="col-5">
+
+                  <div class="text-caption">
+                    New creative collection combining photography,
+                    fashion and minimal editorial design.
+                  </div>
+
+                </div>
+
+              </div>
+
+            </q-card>
+
+          </div>
+
+
+          <!-- PAGE 7 -->
+          <div class="col-12 col-sm-6">
+
+            <q-card class="page-card q-pa-md">
+
+              <div class="text-subtitle1 text-weight-bold">
+                Interiors
+              </div>
+
+              <div class="row q-col-gutter-sm q-mt-md">
+
+                <div class="col-6">
+                  <q-img
+                    :src="Image1"
+                    class="interior-image"
+                    fit="cover"
+                  />
+                </div>
+
+                <div class="col-6">
+                  <q-img
+                    :src="Image1"
+                    class="interior-image"
+                    fit="cover"
+                  />
+                </div>
+
+              </div>
+
+              <div class="text-caption q-mt-md">
+                Clean, minimal and modern design concepts.
+              </div>
+
+            </q-card>
+
+          </div>
+
+
+          <!-- PAGE 8 -->
+          <div class="col-12 col-sm-6">
+
+            <q-card class="page-card q-pa-md">
+
+              <div class="text-subtitle1 text-weight-bold">
+                Best Collection
+              </div>
+
+              <div class="text-caption q-mt-sm">
+                Selected projects from our latest creative works.
+              </div>
+
+              <div class="row q-col-gutter-sm q-mt-md">
+
+                <div class="col-6">
+                  <q-img
+                    :src="Image1"
+                    class="collection-small"
+                    fit="cover"
+                  />
+                </div>
+
+                <div class="col-6">
+                  <q-img
+                    :src="Image1"
+                    class="collection-small"
+                    fit="cover"
+                  />
+                </div>
+
+              </div>
+
+            </q-card>
+
+          </div>
+
+        </div>
 
       </div>
 
@@ -116,107 +342,81 @@
 
 
 <script setup>
-import { ref } from 'vue'
 import Image1 from 'src/assets/Image1.png'
-
-const tab = ref('offices')
 </script>
 
 
 <style lang="sass" scoped>
 
-.left-card
-  height: 770px
+.cover-card
+  min-height: 720px
+  background: white
+  border-radius: 0
+
+.cover-image-area
+  height: 620px
   position: relative
+  overflow: hidden
 
-.right-card
-  height: 770px
-  background: #ff6266
-  position: relative
+.cover-image
+  width: 100%
+  height: 100%
 
-.image-area
+.black-box
   position: absolute
-  top: 150px
-  left: 55%
-  transform: translateX(-50%)
-  width: 500px
-  height: 480px
-
-.circle
-  width: 310px
-  height: 310px
-  background: #ff6266
-  border-radius: 50%
-  position: absolute
-  left: 170px
-  top: 70px
-  z-index: 1
-
-.person-image
-  position: absolute
-  width: 300px
-  height: auto
-  left: 180px
-  top: 50px
+  left: 15px
+  top: 15px
+  bottom: 15px
+  width: 130px
+  background: #111
+  display: flex
+  align-items: center
+  justify-content: center
   z-index: 2
-  transform: scale(1.25)
-  transform-origin: bottom center
 
-.team
-  position: absolute
-  left: 40px
-  top: 330px
-  z-index: 3
-
-
-.title-line
-  width: 105px
-  margin: 9px 0 20px
-  background: #333333
-
-
-.name
-  margin-top: 15px
-  color: grey
-
-
-.job
-  font-size: 20px
-  margin: 8px 0 15px
-
-
-.team a
-  color: #ff6266
-
-
-.login-btn
-  position: absolute
-  top: 30px
-  right: 30px
-  width: 105px
-  height: 40px
-  border-radius: 0 16px 0 16px
-
-
-.view-btn
-  width: 150px
-  height: 48px
-  border-radius: 0 22px 0 22px
-
-
-.content
-  position: absolute
-  top: 170px
-  left: 50px
+.portfolio-title
   color: white
+  font-size: 28px
+  font-weight: 500
+  letter-spacing: 3px
+  writing-mode: vertical-rl
+  transform: rotate(180deg)
 
+.page-card
+  min-height: 300px
+  background: white
+  border-radius: 0
 
-.title
-  font-size: 30px
-  font-weight: bold
+.small-image
+  height: 150px
 
+.medium-image
+  height: 170px
 
-.subtitle
-  margin: 20px 0 35px
+.team-image
+  height: 90px
+
+.large-image
+  height: 170px
+
+.grid-image
+  height: 130px
+
+.collection-image
+  height: 170px
+
+.interior-image
+  height: 160px
+
+.collection-small
+  height: 150px
+
+@media (max-width: 1023px)
+
+  .cover-card
+    min-height: auto
+
+  .cover-image-area
+    height: 600px
 
 </style>
